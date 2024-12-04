bebb7b95862572e34890fb6a70eacffbc2275ca4
Udit <udit.shukla@gonuclei.com>
2024-12-04 17:53:40 +0530

    Revert "First Commit"
    This reverts commit 27ef57acc4aa77ecaad7ab7794bb5c497233be7b.

--- src/main/java/com/dinegood/springbootproject/cofig/Elasticsearchconfig.java
+++
@@ -1,9 +0,0 @@
-//package com.dinegood.springbootproject.cofig;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-//
-//@Configuration
-//@EnableElasticsearchRepositories(basePackages = "com.dinegood.springbootproject.elasticrepo")
-//public class Elasticsearchconfig {
-//}
